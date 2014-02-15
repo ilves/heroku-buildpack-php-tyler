95b480d508af5f79f4ea3d34ba69aa916dfee748
Taavi Ilves <ilves.taavi@gmail.com>
2014-02-15 18:08:45 +0200

    new versions

--- support/set-env.sh
+++ support/set-env.sh
@@ -4,14 +4,14 @@ export S3_BUCKET="socialkit-heroku-buildpack"
 
 export LIBMCRYPT_VERSION="2.5.8"
 export LIFREETYPE_VERSION="2.5.2"
-export PHP_VERSION="5.5.7"
+export PHP_VERSION="5.5.9"
 export APC_VERSION="3.1.13"
-export PHPREDIS_VERSION="2.2.2"
-export LIBMEMCACHED_VERSION="1.0.7"
+export PHPREDIS_VERSION="2.2.4"
+export LIBMEMCACHED_VERSION="1.0.18"
 export MEMCACHED_VERSION="2.1.0"
-export NEWRELIC_VERSION="4.3.5.33"
+export NEWRELIC_VERSION="4.5.5.38"
 
-export NGINX_VERSION="1.5.8"
+export NGINX_VERSION="1.5.10"
 
 export EC2_PRIVATE_KEY=~/.ec2/pk.pem
 export EC2_CERT=~/.ec2/cert.pem
